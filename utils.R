6d65edb655f16d9429c0c04266c26dcb6e3be3af
Jason Everett <jaseeverett@gmail.com>
2022-07-26 20:45:51 +1000

    Consolidate utilities

--- utils.R
+++ utils.R
@@ -1,3 +1,19 @@
+# Harmonic is to fit Hour and DOY
+
+fHarmonic <- function (theta, k = 4) {
+  X <- matrix(0, length(theta), 2 * k)
+  nam <- as.vector(outer(c("c", "s"), 1:k, paste, sep = ""))
+  dimnames(X) <- list(names(theta), nam)
+  m <- 0
+  for (j in 1:k) {
+    X[, (m <- m + 1)] <- cos(j * theta)
+    X[, (m <- m + 1)] <- sin(j * theta)
+  }
+  X
+}
+
+
+# Function to plot the linear models using visreg
 fPlotBiomassLM <- function (mdl, Name) {
   
   # If lm use this
